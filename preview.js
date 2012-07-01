ae4b3993a6066c21462e439c5c91d14f88ef3d11
Seb <github@seb.exse.net>
2012-07-01 07:54:33 +0200

    g3d2 support

--- preview.js
+++ preview.js
@@ -4,7 +4,7 @@
 var io = require('socket.io-client');
 var SDL = require( 'sdl' );
 
-var socket = io.connect('http://bender.hq.c3d2.de:2350');
+var socket = io.connect('http://bender.hq.c3d2.de:2339');
 
 socket.on('disconnect', function () {
 
@@ -40,20 +40,37 @@ socket.on('init', function(configuration) {
 		socket.emit('ack');
 		
 		var buf = data.buf;
-		//console.log(data.ioWindow);			
 		var i = 0;
-		for(i = 0; i < configuration.width;i++)
+		
+		if(configuration.subpixelOrder == 'rrggbb')
 		{
-			var j=0;
-			for(j = 0; j < configuration.height ;j++)
-			{
 			
-				var r = buf.charCodeAt(((23-i)*24+(23-j))*3);
-				var g = buf.charCodeAt(((23-i)*24+(23-j))*3+1);
-				var b = buf.charCodeAt(((23-i)*24+(23-j))*3+2);
+			for(i = 0; i < configuration.height;i++)
+			{
+				var j=0;
+				for(j = 0; j < configuration.width;j++)
+				{
+				
+					var r = buf.charCodeAt(((23-i)*24+(23-j))*3);
+					var g = buf.charCodeAt(((23-i)*24+(23-j))*3+1);
+					var b = buf.charCodeAt(((23-i)*24+(23-j))*3+2);
 
-				SDL.fillRect( screen, [i*factor,(configuration.height-j-1)*factor,factor,factor ], SDL.mapRGB( screen.format,r,g,b ) );
-
+					SDL.fillRect( screen, [j*factor,i*factor,factor,factor ], SDL.mapRGB( screen.format,r,g,b ) );
+				}
+			}
+		}else if (configuration.subpixelOrder == 'g')
+		{
+			for(i = 0; i < configuration.height;i++)
+			{
+				var j=0;
+				for(j = 0; j < configuration.width/2;j++)
+				{
+					var g = buf.charCodeAt( i * (configuration.width/2) + j );
+					var g1 = Math.floor( ( (g & 0x0f)*0x10 )*1.06);
+					var g2 = Math.floor(( g- (g & 0x0f))*1.06);
+					SDL.fillRect( screen, [j*2*factor,i*factor,factor,factor ], SDL.mapRGB( screen.format,0,g1,0 ) );
+					SDL.fillRect( screen, [(j*2+1)*factor,i*factor,factor,factor ], SDL.mapRGB( screen.format,0,g2,0 ) );
+				}
 			}
 		}
 
