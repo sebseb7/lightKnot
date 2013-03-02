2886d5e3a1d58ec0d1576f19d11847eca9a71256
sebseb7 <github@seb.exse.net>
2013-03-02 06:46:09 +0100

    reverted bigalex` pull request because it is broken

--- lightKnot.js
+++ lightKnot.js
@@ -37,24 +37,26 @@ var wallConn = require('./wallConnection.js');
 
 
 {
-	var serialDevice = '/dev/cu.usbserial-A100DDXH';
+	var serialDevice1 = '/dev/cu.usbserial-A8008I0K';
+	//var serialDevice1 = '/dev/cu.usbserial-A100DDXG';
+	//var serialDevice = '/dev/cu.usbserial-A8008I0K';
 		
 	var hardwareAvailable = true;
 
 
 	try{
-		var stats = fs.lstatSync(serialDevice);
-		console.log("running with hardware on " + serialDevice);
+		var stats = fs.lstatSync(serialDevice1);
+		console.log("running with hardware");
 //		fs.watch(serialDevice1, function (event, filename) {
 //			console.log('file event2 : terminate ' + event);
 //			process.exit(0);
 //		});
 	} catch(e) {
 		hardwareAvailable = false;
-		console.log("hardware on " + serialDevice + "not found - running without hardware");
+		console.log("running without hardware");
 		
 		setInterval(function(){
-			fs.lstat(serialDevice,function(err,stats){
+			fs.lstat(serialDevice1,function(err,stats){
 				if(stats && stats.dev)
 				{
 					console.log('restart a');
@@ -64,32 +66,10 @@ var wallConn = require('./wallConnection.js');
 		},5000);
 	}
 
-	var connectionCeil = new wallConn.newConn(hardwareAvailable,serialDevice,500000);
+	var connectionCeil = new wallConn.newConn(hardwareAvailable,serialDevice1,500000);
 
 	var pentawallHD = wall.newWall('PentawallHD',connectionCeil);
+//	var ceilingLED = wall.newWall('CeilingLED',connectionCeil);
 }
-/*{
-	var serialDevice = '/dev/cu.usbserial-AE018X8xS';
-	//var serialDevice = '/dev/cu.usbmodem621';
-		
-	var hardwareAvailable = true;
-
-	try{
-		var stats = fs.lstatSync(serialDevice);
-		console.log("running with hardware");
-	} catch(e) {
-		hardwareAvailable = false;
-		console.log("running without hardware");
-	}
-
-	var g3d2Conn = wallConn.newConn(hardwareAvailable,serialDevice,500000);
-
-	var g3d2 = wall.newWall('g3d2',g3d2Conn);
-}
-*/
-
-
-
-
 
 
--- wallConnection.js
+++ wallConnection.js
@@ -1,4 +1,4 @@
-var serialPort = require('serialport_sebseb7').SerialPort; //needs patch for 500000 baud
+var serialPort = require('serialport').SerialPort; //needs patch for 500000 baud
 
 
 
@@ -198,9 +198,7 @@ exports.newConn = function(realHardwareAvailable,device,baudrate) {
 		if(ledWallConnection){
 			ledWallConnection.write(magic_42);
 			ledWallConnection.write(escapeData(buf),callback,socket);
-		}
-		else
-		{
+		}else{
 			if(callback)
 			{
 				callback('ok');
@@ -216,9 +214,7 @@ exports.newConn = function(realHardwareAvailable,device,baudrate) {
 		if(ledWallConnection){
 			ledWallConnection.write(magic_42);
 			ledWallConnection.write(escapeData(buf),callback,socket);
-		}
-		else
-		{
+		}else{
 			if(callback)
 			{
 				callback('ok');
@@ -233,9 +229,7 @@ exports.newConn = function(realHardwareAvailable,device,baudrate) {
 
 		if(ledWallConnection){
 			ledWallConnection.write(magic_42.concat(escapeData(buf)),callback,socket);
-		}
-		else
-		{
+		}else{
 			if(callback)
 			{
 				callback('ok');
@@ -249,9 +243,7 @@ exports.newConn = function(realHardwareAvailable,device,baudrate) {
 
 		if(ledWallConnection){
 			ledWallConnection.write(magic_42.concat(escapeData(buf)),callback,socket);
-		}
-		else
-		{
+		}else{
 			if(callback)
 			{
 				callback('ok');
@@ -263,12 +255,10 @@ exports.newConn = function(realHardwareAvailable,device,baudrate) {
 	wallConn.setCeiling = function(x,r,g,b,w,callback,socket) {
 
 		var buf = new Buffer([x,r,g,b,w]);
-		console.log(x.toString(16)+' '+r.toString(16)+' '+g.toString(16)+' '+b.toString(16)+' '+w.toString(16));
+
 		if(ledWallConnection){
 			ledWallConnection.write(magic_42.concat(escapeData(buf)),callback,socket);
-		}
-		else
-		{
+		}else{
 			if(callback)
 			{
 				callback('ok');
@@ -282,13 +272,11 @@ exports.newConn = function(realHardwareAvailable,device,baudrate) {
 
 		if(ledWallConnection){
 			ledWallConnection.write(magic_23.concat(escapeData(buf)),callback,socket);
-		}
-		else
-		{
+		}else{
 			if(callback)
 			{
 				callback('ok');
-			}
+			};
 		}
 
 	}
